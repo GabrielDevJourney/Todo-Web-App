c8afc5115de24df552e509fc156d6c1733f31346
GabrielDevJourney <gabspereira178@gmail.com>
2024-09-22 13:27:45 +0100

    sidebar header strcuture done i am for now just building dom and also understanding so of the functionalities i will also need to build like for this header i need my btn to have a ui update event this means to collapse and open again the side bar

--- src/js/components/SidebarHeader.js
+++ src/js/components/SidebarHeader.js
@@ -1,8 +1,8 @@
-import iconLogo from "../../../src/assets/logoToDoHeaderSiderBar.svg";
+import iconLogoSideBar from "../../../src/assets/logoToDoHeaderSiderBar.svg";
+import colapseIcon from "../../../src/assets/sidebarColapseIcon.svg"
+import openSidebarIcon from "../../../src/assets/sidebarOpenIcon.svg"
 
 export function createSidebarHeader(){
-    console.log("icon path", iconLogo);
-
     const logoTextColapseContainer = document.createElement('div')
     logoTextColapseContainer.className = 'logoTextColapseContainer'
 
@@ -12,8 +12,33 @@ export function createSidebarHeader(){
 
     const iconLogoSidebarHeader = document.createElement('img')
     iconLogoSidebarHeader.className = 'iconLogoSidebarHeader'
-    iconLogoSidebarHeader.src = iconLogo
+    iconLogoSidebarHeader.src = iconLogoSideBar;
+    iconLogoSidebarHeader.alt = 'A list icon, color white, with two items of the list checked'
     logoTextContainer.appendChild(iconLogoSidebarHeader)
 
+    const textSideBarHeader = document.createElement('p')
+    textSideBarHeader.className = 'textSideBarHeader'
+    textSideBarHeader.textContent = 'Action'
+    logoTextContainer.appendChild(textSideBarHeader)
+
+    const colapseAndOpenIconsContainer = document.createElement('div')
+    colapseAndOpenIconsContainer.className = 'colapseAndOpenIconsContainer'
+    logoTextColapseContainer.appendChild(colapseAndOpenIconsContainer)
+
+    const colapseBtn = document.createElement('button')
+    colapseBtn.className = 'colapseBtn'
+    colapseBtn.innerHTML = `<img src="${colapseIcon}" alt="Collapse Sidebar">`
+    colapseAndOpenIconsContainer.appendChild(colapseBtn)
+
+    const openSidebarBtn = document.createElement("button");
+	openSidebarBtn.className = "openSidebarBtn";
+	openSidebarBtn.innerHTML = `<img src="${openSidebarIcon}" alt="Open Sidebar">`
+    openSidebarBtn.style.display = 'none'
+	colapseAndOpenIconsContainer.appendChild(openSidebarBtn);
+
     return logoTextColapseContainer
+}
+
+function toggleSidebar(){
+    //TODO
 }
