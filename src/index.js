d06e1cfdbd07e3ae672471cf897944a2a8126546
GabrielDevJourney <gabspereira178@gmail.com>
2024-09-22 13:00:32 +0100

    delete the current implementation has it was just testing to see if the icon was finally being reached

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
 import './styles/main.css'
-import { createSidebarHeader } from './js/components/SidebarHeader'
 
-const SidebarHeader = createSidebarHeader()
-document.body.appendChild(SidebarHeader)
