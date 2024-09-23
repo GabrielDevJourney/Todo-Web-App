d064a2ffb83d38e9a02b1f978850f5a469382537
GabrielDevJourney <gabspereira178@gmail.com>
2024-09-23 10:06:20 +0100

    implement header and calendars view to render themto test them

--- src/js/layouts/Sidebar.js
+++ src/js/layouts/Sidebar.js
@@ -1,1 +1,14 @@
 //I WILL HA TO ASSOCIATE THE FUNCTIONS THAT CREATE COMPONENTS TO VARIABLES AND THEN APPEND THEM TO THEM SIDEBAR MAIN WRAPPER
+import { sidebarHeaderContainer } from "../components/SidebarHeader.js";
+import { sidebarCalendarsViewContainer } from "../components/SidebarCalendarsItem";
+
+const sidebarMainContainer = document.createElement('div')
+sidebarMainContainer.className = 'sidebarMainContainer'
+
+const sidebarHeaderElement = sidebarHeaderContainer
+sidebarMainContainer.appendChild(sidebarHeaderElement)
+
+const sidebarCalendarsViewElement = sidebarCalendarsViewContainer
+sidebarMainContainer.appendChild(sidebarCalendarsViewElement)
+
+export {sidebarMainContainer}
