e4bfd4ae14bff601e520756b9b162b42319a8476
GabrielDevJourney <gabspereira178@gmail.com>
2024-09-25 15:49:08 +0100

    data structure to become eaiser to implement properties to the taskmodal even if there are need more in the future it can be added easily

---
+++ src/js/data/newTaskModalDropItemsManager.js
@@ -0,0 +1,9 @@
+import priorityIcon from "../../../src/assets/pirorityNewTaskModalIcon.svg";
+import dueDateIcon from "../../../src/assets/calendarTomorrowIcon.svg";
+import folderIcon from "../../../src/assets/folderNewTaskModalIcon.svg";
+
+export const dropdownItems = [
+	{ icon: priorityIcon, label: "Priority", type: "priority" },
+	{ icon: dueDateIcon, label: "Due Date", type: "dueDate" },
+	{ icon: folderIcon, label: "List", type: "list" },
+];
