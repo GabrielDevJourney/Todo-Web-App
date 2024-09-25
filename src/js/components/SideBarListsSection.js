bd6f8de664eb163bd8a5cefc16858797d4d5b77a
GabrielDevJourney <gabspereira178@gmail.com>
2024-09-25 15:46:27 +0100

    where lists will be displayed yet some feature to be implemented from getting the actual values from the listmanager.js to display name and color that user choose for the list and also enabling to open those lists tasks in the main content area that is yet to be created

--- src/js/components/SideBarListsSection.js
+++ src/js/components/SideBarListsSection.js
@@ -1,40 +1,39 @@
-import addIcon from '../../../src/assets/addCircleIcon.svg'
-import { getLists, loadListsFromStorage } from '../data/listsManager.js';
-import { createListItem } from './SideBarListItem.js';
+import addIcon from "../../../src/assets/addCircleIcon.svg";
+import { getLists, loadListsFromStorage } from "../data/listsManager.js";
+import { createListItem } from "./SideBarListItem.js";
 
-function createSidebarListSection(){
-    const listSectionContainer = document.createElement('div')
-    listSectionContainer.className = 'listSectionContainer'
+function createSidebarListSection() {
+	const listSectionContainer = document.createElement("div");
+	listSectionContainer.className = "listSectionContainer";
 
-    const listSectionHeaderContainer = document.createElement('div')
-    listSectionHeaderContainer.className = 'listSectionHeaderContainer'
-    listSectionContainer.appendChild(listSectionHeaderContainer)
+	const listSectionHeaderContainer = document.createElement("div");
+	listSectionHeaderContainer.className = "listSectionHeaderContainer";
+	listSectionContainer.appendChild(listSectionHeaderContainer)
 
-    const listHeaderText = document.createElement('p')
-    listHeaderText.className = 'listHeaderText'
-    listHeaderText.textContent = 'Lists'
-    listSectionHeaderContainer.appendChild(listHeaderText)
+	const listHeaderText = document.createElement("p");
+	listHeaderText.className = "listHeaderText";
+	listHeaderText.textContent = "Lists";
+	listSectionHeaderContainer.appendChild(listHeaderText);
 
-    const addNewListBtn = document.createElement('button')
-    addNewListBtn.className = 'addNewListBtn'
-    listSectionHeaderContainer.appendChild(addNewListBtn)
+	const addNewListBtn = document.createElement("button");
+	addNewListBtn.className = "addNewListBtn";
+	listSectionHeaderContainer.appendChild(addNewListBtn);
 
-    const addNewListBtnIcon = document.createElement('img')
-    addNewListBtnIcon.className = 'addNewListBtnIcon'
-    addNewListBtnIcon.src = addIcon
-    addNewListBtnIcon.alt = 'Icon of a plus sign with dotted top left decor'
-    addNewListBtn.appendChild(addNewListBtnIcon)
+	const addNewListBtnIcon = document.createElement("img");
+	addNewListBtnIcon.className = "addNewListBtnIcon";
+	addNewListBtnIcon.src = addIcon;
+	addNewListBtnIcon.alt = "Icon of a plus sign with dotted top left decor";
+	addNewListBtn.appendChild(addNewListBtnIcon);
 
-    const listsView = createListsViewSection()
-    listsView.className = 'listsView'
-    listSectionContainer.appendChild(listsView)
+	const listsView = createListsViewSection();
+	listsView.className = "listsView";
+	listSectionContainer.appendChild(listsView);
 
-    return listSectionContainer
-
+	return listSectionContainer;
 }
 
-function createListsViewSection(){
-    const listsContainer = document.createElement("div");
+function createListsViewSection() {
+	const listsContainer = document.createElement("div");
 	listsContainer.className = "listsContainer";
 
 	function renderLists() {
@@ -49,8 +48,8 @@ function createListsViewSection(){
 	loadListsFromStorage();
 	renderLists();
 
-    return listsContainer
+	return listsContainer;
 }
 
-const sidebarListsSection = createSidebarListSection()
-export {sidebarListsSection}
+const sidebarListsSection = createSidebarListSection();
+export { sidebarListsSection };
