49b18a981ed7c72544645e8bea7a0e6e0367df68
GabrielDevJourney <gabspereira178@gmail.com>
2024-09-25 15:43:40 +0100

    modal to create a new task almost everything created in terms of dom strcuture creation, from modal itself to handling the different type of inputs trough importing arrays and handling function for those same arrays

---
+++ src/js/components/ModalNewTask.js
@@ -0,0 +1,152 @@
+import { prioritiesArray } from "../data/prioritiesManager";
+import { format, addDays } from "date-fns";
+import { updateListDropdowns } from "../ui/modalsHandler";
+import { dropdownItems } from "../data/newTaskModalDropItemsManager";
+
+//export
+function createNewTaskModal() {
+	const addNewTaskModalContainer = document.createElement("div");
+	addNewTaskModalContainer.className = "addNewTaskModalContainer";
+
+	const headerAndExitContainer = document.createElement("div");
+	headerAndExitContainer.className = "headerAndExitContainer";
+	addNewTaskModalContainer.appendChild(headerAndExitContainer);
+
+	const addNewTaskModalHeaderText = document.createElement("p");
+	addNewTaskModalHeaderText.className = "addNewTaskModalHeaderText";
+	addNewTaskModalHeaderText.textContent = "New Task";
+	headerAndExitContainer.appendChild(addNewTaskModalHeaderText);
+
+	const addNewTaskModalExitBtn = document.createElement("button");
+	addNewTaskModalExitBtn.className = "addNewTaskModalExitBtn";
+	headerAndExitContainer.appendChild(addNewTaskModalExitBtn);
+
+	const addNewTaskModalNameAndDescription = document.createElement("div");
+	addNewTaskModalNameAndDescription.className = "addNewTaskModalNameAndDescription";   
+    addNewTaskModalContainer.appendChild(addNewTaskModalNameAndDescription)
+
+	const addNewTaskModalInputTaskName = document.createElement("input");
+	addNewTaskModalInputTaskName.className = "addNewTaskModalInputTaskName";
+	addNewTaskModalNameAndDescription.appendChild(addNewTaskModalInputTaskName);
+
+	const addNewTaskModalInputTaskDescription = document.createElement("input");
+	addNewTaskModalInputTaskDescription.className =
+		"addNewTaskModalInputTaskDescription";
+	addNewTaskModalNameAndDescription.appendChild(
+		addNewTaskModalInputTaskDescription
+	);
+
+	const taskPropertiesContainer = document.createElement("div");
+	taskPropertiesContainer.className = "taskPropertiesContainer";
+    addNewTaskModalContainer.appendChild(taskPropertiesContainer);
+
+	dropdownItems.forEach(item => {
+        const itemCotainer = createItemsContainer(item.icon, item.label, item.type)
+        taskPropertiesContainer.appendChild(itemCotainer)
+        itemCotainer.className = `itemCotainer${item.label}`
+    })
+
+    const newTaskSubmitBtn = document.createElement('button')
+    newTaskSubmitBtn.className = 'newTaskSubmitBtn'
+    newTaskSubmitBtn.type = 'submit'
+    newTaskSubmitBtn.value = 'Add Task'
+
+
+	return addNewTaskModalContainer;
+}
+
+function createItemsContainer(icon, label, inputType) {
+	const newTaskModalItemContainer = document.createElement("div");
+	newTaskModalItemContainer.className = "newTaskModalItemContainer";
+
+	const iconAndTextContainer = createIconLabel(icon, label);
+	newTaskModalItemContainer.appendChild(iconAndTextContainer);
+
+	const inputContainer = createInput(inputType);
+	newTaskModalItemContainer.appendChild(inputContainer);
+
+	return newTaskModalItemContainer;
+}
+
+function createIconLabel(icon, label) {
+	const iconAndLabelContainer = document.createElement("div");
+	iconAndLabelContainer.className = "iconLabelContainer";
+
+	const iconElement = document.createElement("img");
+	iconElement.src = icon;
+	iconElement.alt = label;
+	iconAndLabelContainer.appendChild(iconElement);
+
+	const labelElement = document.createElement("span");
+	labelElement.textContent = label;
+	iconAndLabelContainer.appendChild(labelElement);
+
+	return iconAndLabelContainer;
+}
+
+function createInput(type) {
+	switch (type) {
+		case "priority":
+			return createPriorityDropdown();
+		case "dueDate":
+			return createDatePicker();
+		case "list":
+			return createListDropdown();
+		default:
+			console.log("no lists available");
+            return document.createElement('div')
+	}
+}
+
+function createPriorityDropdown() {
+	const selectDropdown = document.createElement("select");
+	selectDropdown.className = "priorityeDropdown";
+	prioritiesArray.forEach((priority) => {
+		const option = document.createElement("option");
+		option.value = priority.priorityLevel.toLowerCase();
+		option.textContent = priority.priorityLevel;
+		selectDropdown.appendChild(option);
+	});
+	return selectDropdown;
+}
+
+function createDatePicker() {
+	const dateInput = document.createElement("input");
+	dateInput.className = "dateInput";
+	dateInput.type = "date";
+
+	const today = new Date();
+	dateInput.value = format(today, "yyyy-MM-dd");
+
+	dateInput.min = format(today, "yyyy-MM-dd");
+
+	const maxDate = addDays(today, 365);
+	dateInput.max = format(maxDate, "yyyy-MM-dd");
+
+	dateInput.addEventListener("change", (e) => {
+		const selectedDate = new Date(e.target.value);
+		console.log("selected date", format(selectedDate, "dd MMM yyyy"));
+	});
+	return dateInput;
+}
+
+function createListDropdown() {
+	const listsDropdown = document.createElement("select");
+	listsDropdown.className = "listsDropdown";
+
+	const defaultOption = document.createElement("option");
+	defaultOption.value = "";
+	defaultOption.textContent = "Select a list";
+	defaultOption.disabled = true;
+	defaultOption.selected = true;
+	listsDropdown.appendChild(defaultOption);
+
+	updateListDropdowns(listsDropdown);
+
+	return listsDropdown;
+}
+
+//todo add this fucntionality to the exit modal btn
+// function exitAndClearInputsNewTaskModal() {}
+
+export { createNewTaskModal };
