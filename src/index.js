0a995f869b941153f3c091cd1036f1951fc8aa63
GabrielDevJourney <gabspereira178@gmail.com>
2024-09-27 09:45:33 +0100

    loadtasksfromstorage implemention into index to test if it was working it is

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './styles/main.css'
 
 import { sidebarMainContainer } from './js/layouts/Sidebar'
+import { loadTasksFromStorage } from './js/data/tasksManager'
 
+loadTasksFromStorage()
 document.body.appendChild(sidebarMainContainer)
