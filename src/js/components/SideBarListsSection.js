cfb41b590a40ff094a0212fa5c8f9a65ae4c6e1a
GabrielDevJourney <gabspereira178@gmail.com>
2024-09-28 08:49:54 +0100

    func to be exported into modalnewlist so i can render the list when its submited instead of needing to reload page

--- src/js/components/SideBarListsSection.js
+++ src/js/components/SideBarListsSection.js
@@ -1,6 +1,7 @@
 import addIcon from "../../../src/assets/addCircleIcon.svg";
 import { getLists, loadListsFromStorage } from "../data/listsManager.js";
 import { createListItem } from "./SideBarListItem.js";
+import { createNewListModal } from "./ModalNewList.js";
 
 function createSidebarListSection() {
 	const listSectionContainer = document.createElement("div");
@@ -8,15 +9,26 @@ function createSidebarListSection() {
 
 	const listSectionHeaderContainer = document.createElement("div");
 	listSectionHeaderContainer.className = "listSectionHeaderContainer";
-	listSectionContainer.appendChild(listSectionHeaderContainer)
+	listSectionContainer.appendChild(listSectionHeaderContainer);
 
 	const listHeaderText = document.createElement("p");
 	listHeaderText.className = "listHeaderText";
 	listHeaderText.textContent = "Lists";
 	listSectionHeaderContainer.appendChild(listHeaderText);
 
+    let newListModal = null 
+
 	const addNewListBtn = document.createElement("button");
 	addNewListBtn.className = "addNewListBtn";
+	addNewListBtn.addEventListener("click", () => {
+
+        if (!newListModal) {
+			newListModal = createNewListModal();
+			document.body.appendChild(newListModal);
+		}else{
+            newListModal.style.display = 'block'
+        }
+	});
 	listSectionHeaderContainer.appendChild(addNewListBtn);
 
 	const addNewListBtnIcon = document.createElement("img");
@@ -41,15 +53,25 @@ function createListsViewSection() {
 		const lists = getLists();
 		lists.forEach((list) => {
 			const listItem = createListItem(list);
+            listItem.className = 'listItem'
 			listsContainer.appendChild(listItem);
 		});
 	}
 
-	loadListsFromStorage();
+	loadListsFromStorage()
 	renderLists();
 
 	return listsContainer;
 }
+function addNewListItem(list) {
+	const listsView = document.querySelector(".listsView");
+	if (listsView) {
+		const listItem = createListItem(list);
+		listItem.className = "listItem";
+		listsView.appendChild(listItem);
+	}
+}
 
 const sidebarListsSection = createSidebarListSection();
-export { sidebarListsSection };
+
+export { sidebarListsSection, createListItem,addNewListItem };
