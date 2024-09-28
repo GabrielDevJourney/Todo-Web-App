6575e2b84485b07c62da998742d276fb904ab4e5
GabrielDevJourney <gabspereira178@gmail.com>
2024-09-28 08:48:34 +0100

    implmented the who strcuture and most logic behind creating a new list, having into store in a object and then passing the values an imported fucnions that will render list when created and when a page is reloaded trough local storage

---
+++ src/js/components/ModalNewList.js
@@ -0,0 +1,135 @@
+import exitIcon from "../../../src/assets/exitIcon.png";
+import iro from "@jaames/iro";
+import { addNewList, getLists, userLists } from "../data/listsManager";
+import { createListItem } from "./SideBarListItem";
+import { addNewListItem } from "./SideBarListsSection";
+
+function createNewListModal() {
+	const newListModalContainer = document.createElement("div");
+	newListModalContainer.classList = "newListModalContainer";
+
+	const newListModalExitBtnContainer = document.createElement("div");
+	newListModalExitBtnContainer.className = "newListModalExitBtnContainer";
+	newListModalContainer.appendChild(newListModalExitBtnContainer);
+
+	const newListModalExitBtn = document.createElement("button");
+	newListModalExitBtn.className = "newListModalExitBtn";
+	newListModalExitBtn.addEventListener("click", () => {
+		resetModal();
+	});
+	newListModalExitBtnContainer.appendChild(newListModalExitBtn);
+
+	const newListModalExitBtnIcon = document.createElement("img");
+	newListModalExitBtnIcon.className = "newListModalExitBtnIcon";
+	newListModalExitBtnIcon.src = exitIcon;
+	newListModalExitBtnIcon.alt = "Cross icon";
+	newListModalExitBtn.appendChild(newListModalExitBtnIcon);
+
+	const newListModalHeaderContainer = document.createElement("div");
+	newListModalHeaderContainer.className = "newListModalHeaderContainer";
+	newListModalContainer.appendChild(newListModalHeaderContainer);
+
+	const newListModalHeaderText = document.createElement("p");
+	newListModalHeaderText.className = "newListModalHeaderText";
+	newListModalHeaderText.textContent = "New List";
+	newListModalHeaderContainer.appendChild(newListModalHeaderText);
+
+	const newListModalPropertiesContainer = document.createElement("div");
+	newListModalPropertiesContainer.className =
+		"newListModalPropertiesContainer";
+	newListModalContainer.appendChild(newListModalPropertiesContainer);
+
+	const newListModalListNameInput = document.createElement("input");
+	newListModalListNameInput.className = "newListModalListNameInput";
+	newListModalListNameInput.placeholder = "List Name";
+	newListModalPropertiesContainer.appendChild(newListModalListNameInput);
+
+	const newListModalThemeContainer = document.createElement("div");
+	newListModalThemeContainer.className = "newListModalThemeContainer";
+	newListModalThemeContainer.placeholder = "List Name";
+	newListModalPropertiesContainer.appendChild(newListModalThemeContainer);
+
+	const themeText = document.createElement("p");
+	themeText.className = "themeText";
+	themeText.textContent = "Theme";
+	newListModalThemeContainer.appendChild(themeText);
+
+	const colorPickerBtn = document.createElement("button");
+	colorPickerBtn.className = "colorPickerBtn";
+	colorPickerBtn.textContent = "List Color";
+	newListModalThemeContainer.appendChild(colorPickerBtn);
+
+	const colorPickerContainer = document.createElement("div");
+	colorPickerContainer.style.display = "none";
+	newListModalContainer.appendChild(colorPickerContainer);
+
+	const colorPickerElement = document.createElement("div");
+	colorPickerElement.className = "colorPickerElement";
+	colorPickerElement.id = "picker";
+	colorPickerContainer.appendChild(colorPickerElement);
+
+	let colorPicker = null;
+
+	colorPickerBtn.addEventListener("click", () => {
+		colorPickerContainer.style.display =
+			colorPickerContainer.style.display === "none" ? "block" : "none";
+
+		if (!colorPicker) {
+			colorPicker = createColorPicker();
+		}
+	});
+
+	const newListSubmitBtn = document.createElement("button");
+	newListSubmitBtn.className = "newListSubmitBtn";
+	newListSubmitBtn.textContent = "Add New List";
+	newListSubmitBtn.addEventListener("click", () => {
+		const nameOfList = newListModalListNameInput.value;
+		const listTheme = colorPickerBtn.style.backgroundColor;
+		if (addNewList(nameOfList, listTheme)) {
+			const newList = { name: nameOfList, color: listTheme };
+			addNewListItem(newList);
+			resetModal();
+		} else {
+			// Handle the case where the list wasn't added (e.g., duplicate name)
+			alert("List name already exists. Please choose a different name.");
+		}
+	});
+	newListModalContainer.appendChild(newListSubmitBtn);
+
+	return newListModalContainer;
+}
+
+function createColorPicker() {
+	const colorPicker = new iro.ColorPicker("#picker", {
+		width: 100,
+		color: "#f00",
+	});
+
+	colorPicker.on("color:change", function (color) {
+		const colorPickerBtn = document.querySelector(".colorPickerBtn");
+		colorPickerBtn.style.backgroundColor = color.hexString;
+	});
+
+	return colorPicker;
+}
+
+function resetModal() {
+	const newListModalContainerHide = document.querySelector(
+		".newListModalContainer"
+	);
+
+	// Reset input values
+	const clearListNameInput = document.querySelector(
+		".newListModalListNameInput"
+	);
+	clearListNameInput.value = "";
+
+	// Reset color picker
+	const colorPickerBtn = document.querySelector(".colorPickerBtn");
+	colorPickerBtn.style.backgroundColor = "transparent";
+
+	// Hide the modal
+	newListModalContainerHide.style.display = "none";
+}
+
+export { createNewListModal };
