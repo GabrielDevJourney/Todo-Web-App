8ecf0045954f70fc2577fc9fb784eba73aa41275
GabrielDevJourney <gabspereira178@gmail.com>
2024-10-03 11:41:21 +0100

    a simple fucntion to clear tasks when i am testing features

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import './styles/main.css'
 
 import { sidebarMainContainer } from './js/layouts/Sidebar'
-import { loadTasksFromStorage } from './js/data/tasksManager'
+import { loadTasksFromStorage, clearUserTasks } from './js/data/tasksManager'
 
 loadTasksFromStorage()
+
+
 document.body.appendChild(sidebarMainContainer)
+window.clearUserTasks = function () {
+	clearUserTasks();
+	localStorage.removeItem("userTasks");
+	console.log("All user tasks have been cleared");
+	return "Tasks cleared successfully";
+};
