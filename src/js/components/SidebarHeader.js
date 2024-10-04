b79f9c35466804821c4a9913f06799681b7aeac8
GabrielDevJourney <gabspereira178@gmail.com>
2024-10-04 16:15:21 +0100

    calling the collpase fucntion into its proper btn of action

--- src/js/components/SidebarHeader.js
+++ src/js/components/SidebarHeader.js
@@ -1,47 +1,52 @@
 import iconLogoSideBar from "../../../src/assets/logoToDoHeaderSiderBar.svg";
-import colapseIcon from "../../../src/assets/sidebarColapseIcon.svg"
-import openSidebarIcon from "../../../src/assets/sidebarOpenIcon.svg"
+import colapseIcon from "../../../src/assets/sidebarColapseIcon.svg";
+import openSidebarIcon from "../../../src/assets/sidebarOpenIcon.svg";
+import { colapseSidebar } from "./SidebarColapseFeature";
 
-export function createSidebarHeader(){
-    const logoTextColapseContainer = document.createElement('div')
-    logoTextColapseContainer.className = 'logoTextColapseContainer'
+export function createSidebarHeader() {
+	const logoTextColapseContainer = document.createElement("div");
+	logoTextColapseContainer.className = "logoTextColapseContainer";
 
-    const logoTextContainer = document.createElement('div')
-    logoTextContainer.className = 'logoTextContainer'
-    logoTextColapseContainer.appendChild(logoTextContainer)
+	const logoTextContainer = document.createElement("div");
+	logoTextContainer.className = "logoTextContainer";
+	logoTextColapseContainer.appendChild(logoTextContainer);
 
-    const iconLogoSidebarHeader = document.createElement('img')
-    iconLogoSidebarHeader.className = 'iconLogoSidebarHeader'
-    iconLogoSidebarHeader.src = iconLogoSideBar;
-    iconLogoSidebarHeader.alt = 'A list icon, color white, with two items of the list checked'
-    logoTextContainer.appendChild(iconLogoSidebarHeader)
+	const iconLogoSidebarHeader = document.createElement("img");
+	iconLogoSidebarHeader.className = "iconLogoSidebarHeader";
+	iconLogoSidebarHeader.src = iconLogoSideBar;
+	iconLogoSidebarHeader.alt =
+		"A list icon, color white, with two items of the list checked";
+	logoTextContainer.appendChild(iconLogoSidebarHeader);
 
-    const textSideBarHeader = document.createElement('p')
-    textSideBarHeader.className = 'textSideBarHeader'
-    textSideBarHeader.textContent = 'Action'
-    logoTextContainer.appendChild(textSideBarHeader)
+	const textSideBarHeader = document.createElement("p");
+	textSideBarHeader.className = "textSideBarHeader";
+	textSideBarHeader.textContent = "Action";
+	logoTextContainer.appendChild(textSideBarHeader);
 
-    const colapseAndOpenIconsContainer = document.createElement('div')
-    colapseAndOpenIconsContainer.className = 'colapseAndOpenIconsContainer'
-    logoTextColapseContainer.appendChild(colapseAndOpenIconsContainer)
+	const colapseAndOpenIconsContainer = document.createElement("div");
+	colapseAndOpenIconsContainer.className = "colapseAndOpenIconsContainer";
+	logoTextColapseContainer.appendChild(colapseAndOpenIconsContainer);
 
-    const colapseBtn = document.createElement('button')
-    colapseBtn.className = 'colapseBtn'
-    colapseBtn.innerHTML = `<img src="${colapseIcon}" alt="Collapse Sidebar">`
-    colapseAndOpenIconsContainer.appendChild(colapseBtn)
+	const colapseBtn = document.createElement("button");
+	colapseBtn.className = "colapseBtn";
+	colapseBtn.innerHTML = `<img src="${colapseIcon}" alt="Collapse Sidebar">`;
+	colapseBtn.addEventListener("click", () => {
+		if (document.readyState === "complete") {
+			colapseSidebar();
+		} else {
+			window.addEventListener("load", colapseSidebar);
+		}
+	});
+	colapseAndOpenIconsContainer.appendChild(colapseBtn);
 
-    const openSidebarBtn = document.createElement("button");
+	const openSidebarBtn = document.createElement("button");
 	openSidebarBtn.className = "openSidebarBtn";
-	openSidebarBtn.innerHTML = `<img src="${openSidebarIcon}" alt="Open Sidebar">`
-    openSidebarBtn.style.display = 'none'
+	openSidebarBtn.innerHTML = `<img src="${openSidebarIcon}" alt="Open Sidebar">`;
+	openSidebarBtn.style.display = "none";
 	colapseAndOpenIconsContainer.appendChild(openSidebarBtn);
 
-    return logoTextColapseContainer
+	return logoTextColapseContainer;
 }
-const sidebarHeaderContainer = createSidebarHeader()
-sidebarHeaderContainer.className = 'sidebarHeaderContainer'
-export {sidebarHeaderContainer}
-
-function toggleSidebar(){
-    //TODO
-}
+const sidebarHeaderContainer = createSidebarHeader();
+sidebarHeaderContainer.className = "sidebarHeaderContainer";
+export { sidebarHeaderContainer };
