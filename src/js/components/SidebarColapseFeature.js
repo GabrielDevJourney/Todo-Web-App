a4067fb7213810bbd1e2efac66c946e623f67ed1
GabrielDevJourney <gabspereira178@gmail.com>
2024-10-06 07:05:20 +0100

    implemeint a way to change the layout of of color display so i can have the btn and the new icon inside it so when a user hover over the list color he knows he can edit the list and this way i can implement a good ui to it also

--- src/js/components/SidebarColapseFeature.js
+++ src/js/components/SidebarColapseFeature.js
@@ -1,8 +1,27 @@
-
+import listItemEditIconCollapse from "../../../src/assets/listEditIcon_Collapse.svg";
 
 export function colapseSidebar() {
 	const sidebarContainer = document.querySelector(".sidebarMainContainer");
 	sidebarContainer.classList.add("collapsed-sidebar");
+
+	const listColorDisplays = document.querySelectorAll(
+		".listItemColorDisplay"
+	);
+	const listEditBtns = document.querySelectorAll(".listItemEditBtn");
+
+	const listItemEditBtnIcon = document.querySelectorAll(
+		".listItemEditBtnIcon"
+	);
+
+	listColorDisplays.forEach((displayEl, index) => {
+		if (displayEl && listEditBtns[index] && listItemEditBtnIcon[index]) {
+			// Update the icon
+			listItemEditBtnIcon[index].src = listItemEditIconCollapse;
+
+			// Move the edit button
+			displayEl.appendChild(listEditBtns[index]);
+		}
+	});
 }
 
 export function expandSidebar() {
