1cafbacf85570699e1ea3fb427d876ae07662312
GabrielDevJourney <gabspereira178@gmail.com>
2024-10-06 06:05:43 +0100

    implement fucntion to add and remove the class that will change the ui to collapse

--- src/js/components/SidebarColapseFeature.js
+++ src/js/components/SidebarColapseFeature.js
@@ -1,34 +1,11 @@
+
+
 export function colapseSidebar() {
-    const sidebarHeaderTextToHide =
-        document.querySelector(".textSideBarHeader");
-    
-    const sidebarCalendarsText = document.querySelectorAll(".sidebarItemText ");
-    
-    const sidebarListViewSectionHeaderTextToHide =
-        document.querySelector(".listHeaderText");
-    
-    const sidebarListViewListsNameToHide =
-        document.querySelectorAll(".listItemTextName");
-    
-    const colapseBtn = document.querySelector(".colapseBtn");
-    const openSidebarBtn = document.querySelector(".openSidebarBtn");
+	const sidebarContainer = document.querySelector(".sidebarMainContainer");
+	sidebarContainer.classList.add("collapsed-sidebar");
+}
 
-    
-    if(sidebarHeaderTextToHide){
-        sidebarHeaderTextToHide.style.display = 'none'
-    }
-
-	sidebarCalendarsText.forEach((el) => {
-		if (el) el.style.display = "none";
-	});
-
-	if (sidebarListViewSectionHeaderTextToHide)
-		sidebarListViewSectionHeaderTextToHide.style.display = "none";
-
-	sidebarListViewListsNameToHide.forEach((el) => {
-		if (el) el.style.display = "none";
-	});
-
-	if (colapseBtn) colapseBtn.style.display = "none";
-	if (openSidebarBtn) openSidebarBtn.style.display = "block";
+export function expandSidebar() {
+	const sidebarContainer = document.querySelector(".sidebarMainContainer");
+	sidebarContainer.classList.remove("collapsed-sidebar");
 }
