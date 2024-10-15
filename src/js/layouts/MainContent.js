350db8a03002fe13699523c27676c3f94f75efce
GabrielDevJourney <gabspereira178@gmail.com>
2024-10-15 21:21:27 +0100

    importing necessary fucntions to render all maincontent componentes

--- src/js/layouts/MainContent.js
+++ src/js/layouts/MainContent.js
@@ -1,9 +1,17 @@
+import { createFilterTaskContainer } from "../components/MainContentFilterTasks";
 import { createMainContentHeader } from "../components/MainContentHeader";
+import { createTasksViewerContainer } from "../components/MainContentTasksViewer";
 
 const mainContentContainer = document.createElement('div')
 mainContentContainer.className = 'mainContentContainer'
 
 const mainContentHeader = createMainContentHeader()
 mainContentContainer.appendChild(mainContentHeader)
+
+const mainContentFilter = createFilterTaskContainer();
+mainContentContainer.appendChild(mainContentFilter)
+
+const mainContentTasksViewerContainer = createTasksViewerContainer()
+mainContentContainer.appendChild(mainContentTasksViewerContainer)
 
 export{mainContentContainer}
